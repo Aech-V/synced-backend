faf0afb8d63a61b4b5707a6931bdfccae209d2ed
Odekunbi Victor <odekunbivictor2017@gmail.com>
2026-08-22 16:26:33 +0100

    CORS correction

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const server = http.createServer(app);
 app.set('trust proxy', 1);
 
 // Cors Configuration
-const allowedOrigins = [process.env.FRONTEND_URL, 'http://localhost:5173'];
+const allowedOrigins = [process.env.FRONTEND_URL, 'http://localhost:5173', 'https://admin.dynastyinc.com.ng'];
 
 app.use(cors({
     origin: allowedOrigins,
@@ -41,6 +41,10 @@ app.use(
         },
     })
 );
+
+app.get('/', (req, res) => {
+    res.status(200).send('Server is up and running!');
+});
 
 app.use(express.json({ limit: '2mb' }));
 app.use(express.urlencoded({ limit: '2mb', extended: true }));
@@ -92,10 +96,6 @@ socketHandler(io);
 if (typeof startVoidScrubber === 'function') {
     startVoidScrubber();
 }
-
-app.get('/', (req, res) => {
-    res.status(200).send('Server is up and running!');
-});
 
 // Start Listening
 const PORT = process.env.PORT || 5000;
